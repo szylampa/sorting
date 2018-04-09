f87990148a2a4362dbaff431e2d3ccc08e1f5c2c
szylampa <szymonlamparski@gmail.com>
2018-04-09 19:16:27 +0200

    Pierwszy commit

--- src/main/java/com/sda/sorting/BubbleSort.java
+++ src/main/java/com/sda/sorting/BubbleSort.java
@@ -1,7 +1,23 @@
 package com.sda.sorting;
 
 public class BubbleSort {
-    public static void sort(int[] args) {
+    public static void sort(int[] tablica) {
+        int n = tablica.length;
 
+        int licznikOperacji = 0;
+
+        do {
+            for (int i = 0; i < n - 1; i++) {
+                licznikOperacji++;
+                if (tablica[i] > tablica[i + 1]) {
+                    int tymczasowa = tablica[i];
+                    tablica[i] = tablica[i + 1];
+                    tablica[i + 1] = tymczasowa;
+                }
+            }
+            n--;
+        }
+        while (n > 1);
+        System.out.println("Licznik: " + licznikOperacji);
     }
 }
--- src/main/java/com/sda/sorting/Main.java
+++ src/main/java/com/sda/sorting/Main.java
@@ -1,4 +1,15 @@
 package com.sda.sorting;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] tablica = {1,2,3,4,5,6,7,8,9,10};
+        int[] tablica1 = {10,9,8,7,6,5,4,3,2,1};
+
+        System.out.println("Przed" + Arrays.toString(tablica1));
+        BubbleSort.sort(tablica1);
+        System.out.println("Po" + Arrays.toString(tablica1));
+
+    }
 }
